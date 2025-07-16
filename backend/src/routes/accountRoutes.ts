97e498dd614c81a4bbf60c3aeb21201925b904f4
Dean Brown <deanjbrown221@outlook.com>
2025-07-16 18:19:13 +0100

    Add rate limiting using express-rate-limit The ip package required by express-rate-limit is vulnerable to CVE-2024-29415 but the vulnerable code in the ip package is not being used by express-rate-limit. It will however report a warning.
    This issue has a fix proposed: https://github.com/express-rate-limit/express-rate-limit/issues/533
    
    I will update this package as soon as possible.

--- backend/src/routes/accountRoutes.ts
+++ backend/src/routes/accountRoutes.ts
@@ -9,12 +9,14 @@ import {
 import { validateSchemaMiddleware } from "../middleware/validationMiddleware";
 import { userLoginSchema, userRegisterSchema } from "../db/schema/user";
 import { isAuthenticated } from "../middleware/accountMiddleware";
+import { authRateLimit } from "../middleware/rateLimitMiddleware";
 
 // Define account routes
 const accountRoutes: Router = Router();
 // Login Local
 accountRoutes.post(
   "/login/local",
+  authRateLimit,
   validateSchemaMiddleware(userLoginSchema),
   loginLocalController
 );
@@ -24,11 +26,12 @@ accountRoutes.post(
 // Login with Facebook
 accountRoutes.post(
   "/register",
+  authRateLimit,
   validateSchemaMiddleware(userRegisterSchema),
   registerUserController
 );
 accountRoutes.post("/logout", logoutUserController);
-accountRoutes.post("/verify", isAuthenticated, verifyController);
+accountRoutes.post("/verify", authRateLimit, isAuthenticated, verifyController);
 accountRoutes.get("/me", isAuthenticated, userInfoController);
 
 export default accountRoutes;
